5287554ebeda46035bf8796c7c9a433283fd7c5f
Domokos Marcell <domokosmarcelldaniel@gmail.com>
2024-11-26 14:59:34 +0100

    Repository  abstract class is created

--- PrimaryPixels/PrimaryPixels/Program.cs
+++ PrimaryPixels/PrimaryPixels/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using PrimaryPixels.Data;
+using PrimaryPixels.Repositories;
+using PrimaryPixels.Services.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("Default");
@@ -9,6 +11,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 builder.Services.AddDbContext<PrimaryPixelsContext>(options =>
 {
--- PrimaryPixels/PrimaryPixels/Services/Repositories/Repository.cs
+++ PrimaryPixels/PrimaryPixels/Services/Repositories/Repository.cs
@@ -1,6 +1,12 @@
+using PrimaryPixels.Services.Repositories;
+
 namespace PrimaryPixels.Repositories;
 
-public class Repository
+public abstract class Repository<T> : IRepository<T>
 {
-    
+    public abstract IEnumerable<T> GetAll();
+    public abstract T GetById(int id);
+    public abstract int? Add(T entity);
+    public abstract int Update(T entity);
+    public abstract void DeleteById(int id);
 }

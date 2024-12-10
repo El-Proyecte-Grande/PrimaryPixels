4b18acdf6a5c3a8205743f7881da6b9726543846
GogosDani <gogosdaniel04@gmail.com>
2024-12-10 14:02:25 +0100

    Added registration, login, token generation

---
+++ PrimaryPixels/PrimaryPixels/Controllers/AuthController.cs
@@ -0,0 +1,61 @@
+using Microsoft.AspNetCore.Mvc;
+using PrimaryPixels.Contracts;
+using PrimaryPixels.Services.Authentication;
+
+namespace PrimaryPixels.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly IAuthService _authService;
+    public AuthController(IAuthService authService)
+    {
+        _authService = authService;
+    }
+
+    [HttpPost("register")]
+    public async Task<ActionResult<RegistrationResponse>> Register(RegistrationRequest request)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+        var result = await _authService.RegisterAsync(request.Username,request.Email,  request.Password);
+
+        if (!result.Success)
+        {
+            AddErrors(result);
+            return BadRequest(ModelState);
+        }
+
+        return CreatedAtAction(nameof(Register), new RegistrationResponse(result.Email, result.Username));
+    } 
+    
+    [HttpPost("Login")]
+    public async Task<ActionResult<AuthResponse>> Authenticate([FromBody] AuthRequest request)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var result = await _authService.LoginAsync(request.Email, request.Password);
+
+        if (!result.Success)
+        {
+            AddErrors(result);
+            return BadRequest(ModelState);
+        }
+
+        return Ok(new AuthResponse(result.Email, result.Username, result.Token));
+    }
+    
+    private void AddErrors(AuthResult result)
+    {
+        foreach (var error in result.ErrorMessages)
+        {
+            ModelState.AddModelError(error.Key, error.Value);
+        }
+    }
+}
--- PrimaryPixels/PrimaryPixels/Program.cs
+++ PrimaryPixels/PrimaryPixels/Program.cs
@@ -1,4 +1,9 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using PrimaryPixels.Data;
 using PrimaryPixels.Models.Users;
 using PrimaryPixels.Models.Products;
@@ -6,31 +11,22 @@ using PrimaryPixels.Models.Order;
 using PrimaryPixels.Services.Repositories;
 using PrimaryPixels.Models;
 using PrimaryPixels.Models.ShoppingCartItem;
+using PrimaryPixels.Services.Authentication;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("Default");
+var validIssuer = builder.Configuration["TokenValidation:ValidIssuer"];
+var validAudience = builder.Configuration["TokenValidation:ValidAudience"];
+var issuerSigningKey = builder.Configuration["TokenValidation:IssuerSigningKey"];
 // Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<IRepository<User>, UserRepository>();
-builder.Services.AddScoped<IRepository<Headphone>, ProductRepository<Headphone>>();
-builder.Services.AddScoped<IRepository<Phone>, ProductRepository<Phone>>();
-builder.Services.AddScoped<IRepository<Computer>, ProductRepository<Computer>>();
-builder.Services.AddScoped<IRepository<Order>, OrderRepository>();
-builder.Services.AddScoped<IRepository<OrderDetails>, OrderDetailsRepository>();
-builder.Services.AddScoped<IRepository<ShoppingCartItem>, ShoppingCartItemRepository>();
-
-builder.Services.AddDbContext<PrimaryPixelsContext>(options =>
-{
-    options.UseSqlServer(connectionString);
-});
+AddServices();
+ConfigureSwagger();
+AddDbContexts();
+AddAuthentication();
+AddIdentity();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -38,9 +34,109 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
+
+
+void AddServices()
+{
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    builder.Services.AddScoped<IRepository<User>, UserRepository>();
+    builder.Services.AddScoped<IRepository<Headphone>, ProductRepository<Headphone>>();
+    builder.Services.AddScoped<IRepository<Phone>, ProductRepository<Phone>>();
+    builder.Services.AddScoped<IRepository<Computer>, ProductRepository<Computer>>();
+    builder.Services.AddScoped<IRepository<Order>, OrderRepository>();
+    builder.Services.AddScoped<IRepository<OrderDetails>, OrderDetailsRepository>();
+    builder.Services.AddScoped<IRepository<ShoppingCartItem>, ShoppingCartItemRepository>();
+    builder.Services.AddScoped<IAuthService, AuthService>();
+    builder.Services.AddScoped<ITokenService, TokenService>();
+    builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
+}
+
+
+void ConfigureSwagger()
+{
+    builder.Services.AddSwaggerGen(option =>
+    {
+        option.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo API", Version = "v1" });
+        option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            In = ParameterLocation.Header,
+            Description = "Please enter a valid token",
+            Name = "Authorization",
+            Type = SecuritySchemeType.Http,
+            BearerFormat = "JWT",
+            Scheme = "Bearer"
+        });
+        option.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type=ReferenceType.SecurityScheme,
+                        Id="Bearer"
+                    }
+                },
+                new string[]{}
+            }
+        });
+    });
+}
+
+void AddDbContexts()
+{
+    builder.Services.AddDbContext<PrimaryPixelsContext>(options =>
+    {
+        options.UseSqlServer(connectionString);
+    });
+
+    builder.Services.AddDbContext<UsersContext>(options =>
+    {
+        options.UseSqlServer(connectionString);
+
+    });
+}
+
+void AddAuthentication()
+{
+    builder.Services
+        .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters()
+            {
+                ClockSkew = TimeSpan.Zero,
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = validIssuer,
+                ValidAudience = validAudience,
+                IssuerSigningKey = new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(issuerSigningKey)
+                ),
+            };
+        });
+}
+
+void AddIdentity()
+{
+    builder.Services
+        .AddIdentityCore<IdentityUser>(options =>
+        {
+            options.SignIn.RequireConfirmedAccount = false;
+            options.User.RequireUniqueEmail = true;
+            options.Password.RequireDigit = true;
+            options.Password.RequiredLength = 8;
+            options.Password.RequireNonAlphanumeric = false;
+            options.Password.RequireUppercase = true;
+            options.Password.RequireLowercase = true;
+        })
+        .AddEntityFrameworkStores<UsersContext>();
+}

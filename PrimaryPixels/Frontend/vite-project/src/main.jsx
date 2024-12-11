79d3482dec3640ad78c5c1c71879a5856a8cc2df
Domokos Marcell <domokosmarcelldaniel@gmail.com>
2024-12-11 10:56:54 +0100

    creation of routing and page .jsx files

--- PrimaryPixels/Frontend/vite-project/src/main.jsx
+++ PrimaryPixels/Frontend/vite-project/src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />
+  },
+  {
+    path: "/product/:id",
+    element: <ProductPage />
+  },
+  {
+    path: "/cart/:userId",
+    element: <CartPage />
+  }
+])
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+
+  <RouterProvider router={router} />
 )

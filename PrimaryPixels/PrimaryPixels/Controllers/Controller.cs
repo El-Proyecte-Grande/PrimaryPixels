abd6a4ae393c0b94f377873cee662f68a2c35bf3
GogosDani <gogosdaniel04@gmail.com>
2025-01-06 13:56:23 +0100

    merge bug fixed

--- PrimaryPixels/PrimaryPixels/Controllers/Controller.cs
+++ PrimaryPixels/PrimaryPixels/Controllers/Controller.cs
@@ -15,7 +15,7 @@ public abstract class Controller<T> : ControllerBase, IController<T>
         _logger = logger;
         _repository = repository;
     }
-    [HttpPost(""), Authorize(Roles = "Admin")]
+    [HttpPost(""), Authorize]
     [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(int))]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public virtual async Task<IActionResult> Add([FromBody] T entity)
@@ -32,7 +32,7 @@ public abstract class Controller<T> : ControllerBase, IController<T>
             return BadRequest();
         }
     }
-    [HttpDelete("{id}"), Authorize(Roles = "Admin") ]
+    [HttpDelete("{id}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     public virtual async Task<IActionResult> Delete(int id)

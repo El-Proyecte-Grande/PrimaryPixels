89a60086848b23badccee068f35a361aa2015697
Domokos Marcell <domokosmarcelldaniel@gmail.com>
2025-01-09 19:20:26 +0100

    Cart page connected to Home page

--- PrimaryPixels/Frontend/vite-project/src/Components/HomePageComponents/Navbar.jsx
+++ PrimaryPixels/Frontend/vite-project/src/Components/HomePageComponents/Navbar.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import { styled } from 'styled-components';
 import './Navbar.css';
 import { useNavigate } from 'react-router-dom';
+import { jwtDecode } from "jwt-decode";
 
 const StyledNav = styled.nav`
     width: 99.7%;
@@ -20,17 +21,20 @@ const StyledButton = styled.button`
     background-color:rgb(255, 255, 255);
 `;
 
-
+const getUserId = () => {
+    const token = localStorage.getItem("token");
+    const decodedToken = jwtDecode(token);
+    return decodedToken.sub;
+};
 
 function Navbar({isLoggedIn, setIsLoggedIn}){
-    const navigate = useNavigate();
-
+    
     return (
         <StyledNav>
             <img id="logo" src="/primary-pixels-logo.png"/>
             <input type="search" id="searchbar"/>
        <div className='auth-buttons'>
-            {isLoggedIn ? <StyledButton>Cart</StyledButton> : ""}
+            {isLoggedIn ? <StyledButton onClick={() => navigate(`/cart/${getUserId()}`)}>Cart</StyledButton> : ""}
             <StyledButton onClick={() => {!isLoggedIn ? navigate("/login"):  localStorage.removeItem("token"); setIsLoggedIn(false)}}>{!isLoggedIn ? "Login" : "Logout"}</StyledButton>
             {!isLoggedIn ? <StyledButton>Register</StyledButton> : ""}
       </div>

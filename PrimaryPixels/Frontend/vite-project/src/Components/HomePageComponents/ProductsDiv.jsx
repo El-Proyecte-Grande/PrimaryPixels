b602b4a75ea3025a96d8e36ab614b0df9416a97e
GogosDani <gogosdaniel04@gmail.com>
2025-01-22 11:45:05 +0100

    new structure for shoppingCartItem model

--- PrimaryPixels/Frontend/vite-project/src/Components/HomePageComponents/ProductsDiv.jsx
+++ PrimaryPixels/Frontend/vite-project/src/Components/HomePageComponents/ProductsDiv.jsx
@@ -1,4 +1,5 @@
 import './ProductsDiv.css';
+import { jwtDecode } from "jwt-decode";
 import { useNavigate } from 'react-router-dom';
 
 
@@ -7,17 +8,26 @@ import { useNavigate } from 'react-router-dom';
 
 function ProductsDiv({ products }) {
     const navigate = useNavigate();
+
+    async function addToCart(productId) {
+
+        const token = localStorage.getItem("token");
+        const decodedToken = jwtDecode(token);
+        console.log(decodedToken.sub);
+        console.log(productId)
+    }
+
     return (
         <div id="products-container">
             {products.map((product, index) => (
-                <div key={index} className='product' onClick={() => navigate(`/product/${product.id}`)}>
-                    <img className='product-image' src={product.image} alt={`This would be a picture of ${product.name}`}></img>
+                <div key={index} className='product' >
+                    <img className='product-image' onClick={() => navigate(`/product/${product.id}`)} src={product.image} alt={`This would be a picture of ${product.name}`}></img>
                     <div className="product-infos">
                         <p className="product-info"> {product.name} </p>
                         <div className='finance'>
                             <p className="product-info"> {product.price} HUF</p>
                             <div>
-                                <button className='add-to-cart-button'> ADD </button>
+                                <button onClick={() => addToCart(product.id)} className='add-to-cart-button'> ADD </button>
                             </div>
                         </div>
                     </div>

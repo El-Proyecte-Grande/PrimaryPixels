e12e110f9b664c7f9a2fc0b5985fa9df4ead063d
GogosDani <gogosdaniel04@gmail.com>
2025-02-05 09:39:53 +0100

    fetch orderDetails FE

--- PrimaryPixels/Frontend/vite-project/src/Pages/OrderDetails.jsx
+++ PrimaryPixels/Frontend/vite-project/src/Pages/OrderDetails.jsx
@@ -1,4 +1,25 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom"
+import { apiWithAuth } from "../Axios/api";
+
 export default function OrderDetails() {
+
+    const { orderId } = useParams();
+    const [orderDetails, setOrderDetails] = useState([]);
+
+    useEffect(() => {
+        async function getOrderDetails() {
+            const response = await apiWithAuth.get(`/api/orderDetails/order/${orderId}`)
+            const data = await response.data;
+            setOrderDetails(data);
+        }
+        getOrderDetails();
+    }, [])
+
+    useEffect(() => {
+        console.log(orderDetails);
+    }, [orderDetails])
+
     return (
         <>
             <h1> ORDERDETAILS </h1>

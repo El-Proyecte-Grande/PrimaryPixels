f3d23b456596086d100b7d68dfd95628f51af7c4
GogosDani <gogosdaniel04@gmail.com>
2025-02-05 10:43:53 +0100

    orderDetails page

--- PrimaryPixels/Frontend/vite-project/src/Pages/OrderDetails.jsx
+++ PrimaryPixels/Frontend/vite-project/src/Pages/OrderDetails.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom"
+import { useParams, useNavigate } from "react-router-dom"
 import { apiWithAuth } from "../Axios/api";
+import "./OrderDetails.scss"
 
 export default function OrderDetails() {
 
     const { orderId } = useParams();
+    const navigate = useNavigate();
     const [orderDetails, setOrderDetails] = useState([]);
 
     useEffect(() => {
@@ -16,13 +18,47 @@ export default function OrderDetails() {
         getOrderDetails();
     }, [])
 
-    useEffect(() => {
-        console.log(orderDetails);
-    }, [orderDetails])
 
     return (
         <>
-            <h1> ORDERDETAILS </h1>
+            <button onClick={() => navigate(-1)} className="back-button"> &#10094; back </button>
+            <h2 className="order-number"> ORDER #{orderId}</h2>
+            <table className="orders">
+                <thead className="orders-details-table-head">
+                    <tr className="orders-details">
+                        <th> </th>
+                        <th> Product Name </th>
+                        <th> Quantity </th>
+                        <th> Unit Price </th>
+                        <th> Total Price </th>
+                    </tr>
+                </thead>
+                <tbody className="orders-details-table-body">
+                    {orderDetails.map((od) =>
+                        <tr className="order-details" key={od.id}>
+                            <td> <img src={od.product.image} className="order-details-image" /> </td>
+                            <td> {od.product.name} </td>
+                            <td> {od.quantity} </td>
+                            <td> {formatHUF(od.unitPrice)} </td>
+                            <td> {formatHUF(od.totalPrice)} </td>
+                        </tr>
+
+                    )}
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td className="total-price" > Total: {formatHUF(orderDetails.reduce((acc, curr) => acc + curr.totalPrice, 0))}</td>
+                    </tr>
+                </tbody>
+            </table >
         </>
     )
+}
+
+function formatHUF(amount) {
+    const amountStr = amount.toString();
+    const formattedAmount = amountStr.replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+    return `${formattedAmount} HUF`;
 }

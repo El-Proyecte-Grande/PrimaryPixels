0650fb98ae1c2bf5c0b847b063475b538033ef03
GogosDani <gogosdaniel04@gmail.com>
2025-02-16 10:16:22 +0100

    BE: products search controller + repo method

--- PrimaryPixels/PrimaryPixels/Services/Repositories/IProductRepository.cs
+++ PrimaryPixels/PrimaryPixels/Services/Repositories/IProductRepository.cs
@@ -7,4 +7,5 @@ public interface IProductRepository
     public Task<IEnumerable<Product>> GetAll();
     public Task<Product> GetById(int id);
     public Task<IEnumerable<Product>> GetPopular();
+    public Task<IEnumerable<Product>> Search(string word);
 }

7e0c72de402c89eb9fbb4b8b6d44991b0ec68e63
majorosgereby <25921022+majorosgereby@users.noreply.github.com>
2025-02-27 15:23:29 +0100

    add frontend server dockerfile

--- PrimaryPixels/Frontend/Server/server.js
+++ PrimaryPixels/Frontend/Server/server.js
@@ -4,19 +4,19 @@ import { fileURLToPath } from "url";
 import { dirname, join } from "path";
 
 const app = express();
-const PORT = process.env.PORT || 4000; // Use environment port or default to 8128
+const PORT = process.env.PORT || 4000;
 
 // Get __dirname equivalent in ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-// Serve static files from the React app
-app.use(express.static(join(__dirname, "dist"))); // Assuming 'dist' is where your build files are located
-
 // Use morgan middleware to log requests
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 
-// The "catchall" handler: for any request that doesn't match one above, send back index.html.
+// Serve static files from the entire "dist" folder
+app.use(express.static(join(__dirname, "dist")));
+
+// The "catchall" handler for React routing
 app.get("*", (req, res) => {
     res.sendFile(join(__dirname, "dist", "index.html"));
 });

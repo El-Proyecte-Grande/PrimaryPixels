d2548fb303a35b44aedb4627d8aed19c37d5ec0f
GogosDani <gogosdaniel04@gmail.com>
2025-02-27 16:19:33 +0100

    Migration: PaymentIntentId column to order

--- PrimaryPixels/PrimaryPixels/Migrations/20250227124200_Add PaymentIntentId column to order.cs
+++ PrimaryPixels/PrimaryPixels/Migrations/20250227124200_Add PaymentIntentId column to order.cs
@@ -10,27 +10,12 @@ namespace PrimaryPixels.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.AlterColumn<string>(
+            migrationBuilder.AddColumn<string>(
                 name: "PaymentIntentId",
                 table: "Orders",
                 type: "nvarchar(max)",
-                nullable: true,
-                oldClrType: typeof(string),
-                oldType: "nvarchar(max)");
-        }
-
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AlterColumn<string>(
-                name: "PaymentIntentId",
-                table: "Orders",
-                type: "nvarchar(max)",
-                nullable: false,
-                defaultValue: "",
-                oldClrType: typeof(string),
-                oldType: "nvarchar(max)",
-                oldNullable: true);
+                nullable: true
+            );
         }
     }
 }

b4e75f69e1b18323a760cff751a55e36ef2dd271
majorosgereby <25921022+majorosgereby@users.noreply.github.com>
2025-03-03 14:12:46 +0100

    add backend deplyoment and service

--- PrimaryPixels/deployment/kubectl_commands.sh
+++ PrimaryPixels/deployment/kubectl_commands.sh
@@ -3,3 +3,7 @@
 # Apply namespaces
 kubectl apply -f namespace-app.yaml
 kubectl apply -f namespace-db.yaml
+
+# Apply deployments
+kubectl apply -f deployment-frontend.yaml
+kubectl apply -f deployment-backend.yaml

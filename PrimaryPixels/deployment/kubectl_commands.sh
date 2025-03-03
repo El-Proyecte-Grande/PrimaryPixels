ddeca0a462b2164d47c4334f3f47dddf3876a14a
majorosgereby <25921022+majorosgereby@users.noreply.github.com>
2025-03-03 14:24:35 +0100

    add frontend service

--- PrimaryPixels/deployment/kubectl_commands.sh
+++ PrimaryPixels/deployment/kubectl_commands.sh
@@ -7,3 +7,7 @@ kubectl apply -f namespace-db.yaml
 # Apply deployments
 kubectl apply -f deployment-frontend.yaml
 kubectl apply -f deployment-backend.yaml
+
+# apply services
+kubectl apply -f service-backend.yaml
+kubectl apply -f service-frontend.yaml

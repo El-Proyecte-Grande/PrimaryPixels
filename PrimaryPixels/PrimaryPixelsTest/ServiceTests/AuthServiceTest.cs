8f9c7632129e5893d9dda92a225aa6ae1bf97cc1
Domokos Marcell <domokosmarcelldaniel@gmail.com>
2025-03-12 18:40:27 +0100

    AuthServiceTest Setup

---
+++ PrimaryPixels/PrimaryPixelsTest/ServiceTests/AuthServiceTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Moq;
+using PrimaryPixels.Services.Authentication;
+
+namespace PrimaryPixelsTest.ServiceTests
+{
+    [TestFixture]
+    public class AuthServiceTest
+    {
+        private AuthService _authService;
+        private Mock<UserManager<IdentityUser>> _userManagerMock;
+        private Mock<ITokenService> _mockTokenService; 
+        private Mock<UserManager<IdentityUser>> GetMockUserManager()
+        {
+            var store = new Mock<IUserStore<IdentityUser>>();
+            return new Mock<UserManager<IdentityUser>>(store.Object, null, null, null, null, null, null, null, null);
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            _userManagerMock = GetMockUserManager();
+            _mockTokenService = new();
+            _authService = new(_userManagerMock.Object, _mockTokenService.Object);
+        }
+
+
+    }
+}
